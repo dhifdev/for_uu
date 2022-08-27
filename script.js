75f00a3e4c4a40c407eb0eb45f0901fae00ae758
DivDev <adhiviajulian@gmail.com>
2022-08-27 22:55:32 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const checkbox = document.getElementById("checkbox");
+
+checkbox.addEventListener("change", () => {
+  // change the theme of the website
+  document.body.classList.toggle("dark");
+});
+
+// const checkbox = document.getElementById("checkbox");
+// const text = document.getElementById("type");
+
+// checkbox.addEventListener("click", () => {
+//   text.innerHTML = "Love u";
+//   text.style.fontSize = "400px";
+// });
